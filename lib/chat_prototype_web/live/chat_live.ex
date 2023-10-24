8e9c4556312712730abc7e33f46e32c5000499cb
Rui Fernandes <rui.cruz.fernandes@hotmail.com>
2023-10-24 18:20:38 +0100

    Add text input

--- lib/chat_prototype_web/live/chat_live.ex
+++ lib/chat_prototype_web/live/chat_live.ex
@@ -2,7 +2,11 @@ defmodule ChatPrototypeWeb.ChatLive do
   use ChatPrototypeWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, messages: [%{user: "Chatbot", text: "Welcome to my chat!"}])}
+    {:ok,
+     assign(socket,
+       messages: [%{user: "Chatbot", text: "Welcome to my chat!"}],
+       form: to_form(%{user_input: "", text_input: ""})
+     )}
   end
 
   def render(assigns) do
@@ -15,6 +19,13 @@ defmodule ChatPrototypeWeb.ChatLive do
     <% end %>
 
     <.button phx-click="more">Tell me more</.button>
+
+    <.simple_form for={@form} phx-submit="send_message">
+      <b> Write something! </b>
+      <p>User name: <.input field={@form[:user]} value={@form.params.user_input} /></p>
+      <p>Your message: <.input field={@form[:text]} value={@form.params.text_input} /></p>
+      <.button>Send message</.button>
+    </.simple_form>
     """
   end
 
@@ -23,6 +34,13 @@ defmodule ChatPrototypeWeb.ChatLive do
      assign(socket,
        messages:
          Enum.concat(socket.assigns.messages, [%{user: "Chatbot", text: "What do you need?"}])
+     )}
+  end
+
+  def handle_event("send_message", %{"user" => user, "text" => text}, socket) do
+    {:noreply,
+     assign(socket,
+       messages: Enum.concat(socket.assigns.messages, [%{user: user, text: text}])
      )}
   end
 end
