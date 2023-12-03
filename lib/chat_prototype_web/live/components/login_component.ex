650c59ca8ee62f6c257916bf0b4959289682a0cb
Rui Fernandes <rui.cruz.fernandes@hotmail.com>
2023-12-03 23:35:23 +0000

    Fix login UI

--- lib/chat_prototype_web/live/components/login_component.ex
+++ lib/chat_prototype_web/live/components/login_component.ex
@@ -4,14 +4,16 @@ defmodule ChatPrototypeWeb.LoginComponent do
   @spec render_login(Socket.assigns()) :: Phoenix.LiveView.Rendered.t()
   def render_login(assigns) do
     ~H"""
-    <div>
-      <p class="text-2xl">Welcome to chat</p>
+    <div class="bg-zinc-600 grow p-10 text-gray-100">
+      <p class="text-4xl border-b-2">Welcome to this chat app!</p>
 
-      <.simple_form id="login_form" class="mt-40" for={@form} phx-submit="sign_in">
-        <b>Please set your username</b>
-        <p>User name: <.input field={@form["user_name"]} value={@form.params["user_name"]} /></p>
-        <.button>Sign in</.button>
-      </.simple_form>
+      <div class="mt-10 w-80">
+        <.simple_form id="login_form" for={@form} phx-submit="sign_in">
+          <b>Please set your username</b>
+          <p>User name: <.input field={@form["user_name"]} value={@form.params["user_name"]} /></p>
+          <.button>Sign in</.button>
+        </.simple_form>
+      </div>
     </div>
     """
   end
