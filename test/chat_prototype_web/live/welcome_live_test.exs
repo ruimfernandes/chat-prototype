478374d041c652e68cef3d6fe92cdacb1e5aa22e
Rui Fernandes <rui.cruz.fernandes@hotmail.com>
2023-12-04 01:13:23 +0000

    Fix LiveView tests

--- test/chat_prototype_web/live/welcome_live_test.exs
+++ test/chat_prototype_web/live/welcome_live_test.exs
@@ -2,6 +2,10 @@ defmodule ChatPrototypeWeb.WelcomeLiveTest do
   use ChatPrototypeWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import Mock
+
+  alias ChatPrototype.Server
+  alias ChatPrototype.Server.Room
 
   describe "index" do
     test "shows menu asking for user name", %{conn: conn} do
@@ -21,74 +25,127 @@ defmodule ChatPrototypeWeb.WelcomeLiveTest do
     end
 
     test "joins a room from the main room", %{conn: conn} do
-      {:ok, view, html} = live(conn, "/")
+      with_mock Server,
+        list_rooms: fn ->
+          [
+            %{uuid: "room-1", name: "Cars"},
+            %{uuid: "room-2", name: "Football"},
+            %{uuid: "room-3", name: "Surf"}
+          ]
+        end do
+        {:ok, view, html} = live(conn, "/")
 
-      refute has_element?(view, "#menu-room-3", "Surf")
-
-      result =
         view
-        |> element("button", "Surf")
-        |> render_click()
+        |> form("#login_form", %{
+          "user_name" => "Rui Fernandes"
+        })
+        |> render_submit()
 
-      assert result =~ "Surf (already joinned)"
+        refute has_element?(view, "#menu-room-3", "Surf")
 
-      assert has_element?(view, "#menu-room-3", "Surf")
+        result =
+          view
+          |> element("button", "Surf")
+          |> render_click()
+
+        assert result =~ "Surf (already joinned)"
+
+        assert has_element?(view, "#menu-room-3", "Surf")
+      end
     end
 
     test "shows a different chat room upon clicking the side menu button", %{conn: conn} do
-      {:ok, view, html} = live(conn, "/")
+      with_mocks [
+        {Server, [],
+         list_rooms: fn ->
+           [
+             %{uuid: "room-1", name: "Cars"},
+             %{uuid: "room-2", name: "Football"},
+             %{uuid: "room-3", name: "Surf"}
+           ]
+         end},
+        {Room, [],
+         get_messages: fn _room_name -> [%{uuid: "0", user: "TestUser", text: "Hello"}] end}
+      ] do
+        {:ok, view, html} = live(conn, "/")
 
-      assert has_element?(view, "#main-room")
-      refute has_element?(view, "#chat-messages")
-      refute has_element?(view, "#menu-room-3", "Surf")
-
-      result =
         view
-        |> element("button", "Surf")
+        |> form("#login_form", %{
+          "user_name" => "Rui Fernandes"
+        })
+        |> render_submit()
+
+        assert has_element?(view, "#main-room")
+        refute has_element?(view, "#chat-messages")
+        refute has_element?(view, "#menu-room-3", "Surf")
+
+        result =
+          view
+          |> element("button", "Surf")
+          |> render_click()
+
+        assert result =~ "Surf (already joinned)"
+
+        assert has_element?(view, "#menu-room-3", "Surf")
+
+        view
+        |> element("#menu-room-3", "Surf")
         |> render_click()
 
-      assert result =~ "Surf (already joinned)"
-
-      assert has_element?(view, "#menu-room-3", "Surf")
-
-      view
-      |> element("#menu-room-3", "Surf")
-      |> render_click()
-
-      refute has_element?(view, "#main-room")
-      assert has_element?(view, "#chat-messages")
+        refute has_element?(view, "#main-room")
+        assert has_element?(view, "#chat-messages", "TestUser - Hello")
+      end
     end
 
     test "shows new incoming messages on the selected chat room", %{conn: conn} do
-      {:ok, view, html} = live(conn, "/")
+      with_mocks [
+        {Server, [],
+         list_rooms: fn ->
+           [
+             %{uuid: "room-1", name: "Cars"},
+             %{uuid: "room-2", name: "Football"},
+             %{uuid: "room-3", name: "Surf"}
+           ]
+         end},
+        {Room, [],
+         get_messages: fn _room_name -> [%{uuid: "0", user: "TestUser", text: "Hello."}] end}
+      ] do
+        {:ok, view, html} = live(conn, "/")
 
-      assert has_element?(view, "#main-room")
-      refute has_element?(view, "#chat-messages")
-      refute has_element?(view, "#menu-room-3", "Surf")
-
-      result =
         view
-        |> element("button", "Surf")
+        |> form("#login_form", %{
+          "user_name" => "Rui Fernandes"
+        })
+        |> render_submit()
+
+        assert has_element?(view, "#main-room")
+        refute has_element?(view, "#chat-messages")
+        refute has_element?(view, "#menu-room-3", "Surf")
+
+        result =
+          view
+          |> element("button", "Surf")
+          |> render_click()
+
+        assert result =~ "Surf (already joinned)"
+
+        assert has_element?(view, "#menu-room-3", "Surf")
+
+        view
+        |> element("#menu-room-3", "Surf")
         |> render_click()
 
-      assert result =~ "Surf (already joinned)"
+        refute has_element?(view, "#main-room")
+        assert has_element?(view, "#chat-messages")
 
-      assert has_element?(view, "#menu-room-3", "Surf")
+        ChatPrototypeWeb.Endpoint.broadcast("room-3", "new-message", %{
+          uuid: UUID.uuid4(),
+          user: "Test user",
+          text: "testing message"
+        })
 
-      view
-      |> element("#menu-room-3", "Surf")
-      |> render_click()
-
-      refute has_element?(view, "#main-room")
-      assert has_element?(view, "#chat-messages")
-
-      ChatPrototypeWeb.Endpoint.broadcast("room-3", "new-message", %{
-        uuid: UUID.uuid4(),
-        user: "Test user",
-        text: "testing message"
-      })
-
-      assert has_element?(view, "#chat-messages", "Test user - testing message")
+        assert has_element?(view, "#chat-messages", "Test user - testing message")
+      end
     end
 
     test "TODO: shows messages other users join or leave the selected chat room", %{conn: conn} do
